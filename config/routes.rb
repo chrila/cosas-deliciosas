dbd00b8697fcfffc97567eb2fb7e36390afc644e
Christian Lampl <christian.lampl@gmx.at>
2021-02-22 21:21:45 -0300

    Add: integrate bootstrap in views - breweries, currencies, serving formats

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'tastings#index'
 end

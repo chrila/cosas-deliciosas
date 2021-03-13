8c060e5df84eca2f031244cdd4b8c02860fd0cac
Christian Lampl <christian.lampl@gmx.at>
2021-03-12 22:05:42 -0300

    Add: a few validations

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -3,6 +3,11 @@ class Tasting < ApplicationRecord
   belongs_to :beer
   belongs_to :currency
 
+  validates :beer, presence: true
+  validates :place, presence: true
+  validates :price, presence: true
+  validates :rating, presence: true
+
   def rating_str
     '%.1f' % rating
   end
